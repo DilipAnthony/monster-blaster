96e9dd0bca1ccbabe6a2e3649e20684ad1304467
dilipanthony <dilippro@ymail.com>
2020-05-23 14:37:51 +0530

    Updates

---
+++ precache-manifest.7c48138019d479499be9a2c49622bc21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e772621d419f367c6ea617df81e99de5",
+    "url": "/monster-blaster/index.html"
+  },
+  {
+    "revision": "5cbf08354d036ed9a4e0",
+    "url": "/monster-blaster/static/css/main.dd1ffcc8.chunk.css"
+  },
+  {
+    "revision": "375400e9424531da2cc7",
+    "url": "/monster-blaster/static/js/2.7280c166.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-blaster/static/js/2.7280c166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cbf08354d036ed9a4e0",
+    "url": "/monster-blaster/static/js/main.fed76545.chunk.js"
+  },
+  {
+    "revision": "1bf14b2195f3430ed185",
+    "url": "/monster-blaster/static/js/runtime-main.291ba206.js"
+  }
+]);
